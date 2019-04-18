2e46ace4933fec7f7c2122e37fccd27a9c45a790
Mikaner <c0118259dc@edu.teu.ac.jp>
2019-04-18 20:18:08 +0900

    add file

---
+++ takeoff.py
@@ -0,0 +1,15 @@
+import socket
+import json
+
+with open("./ip.json",'r',encoding="utf-8") as code:
+    config = code
+
+TELLO_IP = config["ip"]
+TELLO_PORT = config["port"]
+
+tello_address = (TELLO_IP, TELLO_PORT)
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+socket.sendto("command".encode('utf-8'), tello_address)
+socket.sendto("takeoff".encode('utf-8'), tello_address)
